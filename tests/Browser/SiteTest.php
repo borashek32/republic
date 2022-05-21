8b5295ac180ef3389fe89e92c2391aeea4b9c46d
borashek32 <borashek@inbox.ru>
2022-05-21 23:50:03 +0300

    dusk yeah))))

---
+++ tests/Browser/SiteTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\Post;
+
+class ExampleTest extends DuskTestCase
+{
+    public function testMainPage()
+    {
+        $this->post = Post::where('visability', 1)->first();
+        
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->pause(1000)
+                ->assertDontSee('123')
+                ->assertSee($this->post->title)
+                ->assertDontSee('logged users')
+                ->assertSee('Posts List');
+        });
+    }
+
+    public function testOnePostPage()
+    {
+        $this->post = Post::where('visability', 1)->first();
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/' . $this->post->id)
+                ->pause(1000)
+                ->assertDontSee('123')
+                ->assertSee($this->post->title)
+                ->assertSee('*Public post')
+                ->assertDontSee('logged users')
+                ->assertSee($this->post->description);
+        });
+    }
+
+    public function testLoginPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->pause(1000)
+                ->assertDontSee('123')
+                ->assertSee('Remember me');
+        });
+    }
+}
